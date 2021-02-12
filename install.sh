759ba471ed612728a469be283087ce613d4e36fb
Jordan Schneidler <jordan.schneidler@gmail.com>
2021-02-12 15:56:31 -0700

    Updated dependency install script

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Install dependencies
-sudo dpkg --add-architecture i386; sudo apt update; sudo apt install curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 steamcmd
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 steamcmd
 
 # Install server
 mkdir ~/valheim_server
