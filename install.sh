739fe15c1701857dd34f87c1921feb2b1f857db8
Jordan Schneidler <jordan.schneidler@gmail.com>
2021-02-12 18:09:41 -0700

    Added missing dependency

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ script_dir=$(pwd)
 # Install dependencies
 sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 steamcmd
+sudo apt install -y curl wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 steamcmd libsdl2-2.0-0:i386
 
 # Install server
 mkdir ~/valheim_server
